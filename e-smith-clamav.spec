f7f104c86069d15e9efeae6bb96cb9f8e8490202
Shad L. Lords <slords@mail.com>
2005-07-26 15:20:47 +0000

    Initial revision

---
+++ e-smith-clamav.spec
@@ -0,0 +1,145 @@
+Summary: e-smith module to configure clamav
+%define name e-smith-clamav
+Name: %{name}
+%define version 1.1.0
+%define release 04
+Version: %{version}
+Release: %{release}
+License: GPL
+Group: System Environment/Base
+Source: %{name}-%{version}.tar.gz
+Packager: e-smith developers <bugs@e-smith.com>
+BuildRoot: /var/tmp/%{name}-%{version}-%{release}-buildroot
+BuildArchitectures: noarch
+Requires: e-smith-lib
+Requires: clamav >= 0.83
+Requires: clamd >= 0.83
+Requires: clamav-db
+BuildRequires: e-smith-devtools
+
+%description
+e-smith server enhancement to configure and run clamd and
+freshclam
+
+%changelog
+* Fri Jul 22 2005 Charlie Brady <charlieb@e-smith.com>
+- [1.1.0-04]
+- Fix typo in en-us lexicon. [SF: 1242585]
+
+* Mon May 30 2005 Charlie Brady <charlieb@e-smith.com>
+- [1.1.0-03]
+- Fix last-updated section and labels [Gordon: SF-1200428]
+
+* Tue May 17 2005 Charlie Brady <charlieb@e-smith.com>
+- [1.1.0-02]
+- Add clamav-update event directory.
+- Modify some of the panel code to avoid logging of multiple db property
+  transactions.
+
+* Fri May 13 2005 Gordon Rowell <gordonr@gormand.com.au>
+- [1.1.0-01gr06]
+- Read the timestamp of the clamav database files in the panel
+  to show the last updated time [SF:1200428]
+- Change /sbin/e-smith/freshclam-update-ok to /bin/sh, with no
+  content. 
+- TODO: make the update mails configurable
+
+* Fri May 6 2005 Gordon Rowell <gordonr@gormand.com.au>
+- [1.1.0-01gr05]
+- And hide the sections
+
+* Fri May 6 2005 Gordon Rowell <gordonr@gormand.com.au>
+- [1.1.0-01gr04]
+- Fix up section bars
+
+* Fri May 6 2005 Gordon Rowell <gordonr@gormand.com.au>
+- [1.1.0-01gr03]
+- Removed the qmailscan_integration part of the panel
+- Removed the database mirror select box. Anyone who isn't
+  satisfied with using db.local.clamav.net can set the hostname
+  in the database directly.
+- Hid the updates and proxy sections, based on 
+  $clamav{ShowProxySection} and $clamav{ShowUpdatesSection}
+  - Panel looks rather bare now
+- Read the last updated date from $clamav{SignaturesUpdated}
+- Modified /sbin/e-smith/freshclam-update-ok to set the above value
+- Morphed /etc/clamscan template into /sbin/e-smith/clamscan,
+  which can read the db directly.
+
+* Fri May 6 2005 Gordon Rowell <gordonr@gormand.com.au>
+- [1.1.0-01gr02]
+- Use db.local.clamav.net by default (rather than db.us).
+
+* Fri May 6 2005 Gordon Rowell <gordonr@gormand.com.au>
+- [1.1.0-01gr01]
+- Bump version to 1.1.0-01gr01 to upgrade over Shad's 1.0.0
+- Roll new tarball after major merge of Shad's work.
+
+* Fri May 6 2005 Gordon Rowell <gordonr@gormand.com.au>
+- [0.0.1-06gr02]
+- Cleaned up patch and startup symlinks
+
+* Fri May 6 2005 Gordon Rowell <gordonr@gormand.com.au>
+- [0.0.1-06gr01]
+- Merge in Shad Lords' e-smith-spamassassin work (based in part
+  on work from Damien Curtain) and panel
+- Moved freshclam run script options to database defaults
+- Created /sbin/e-smith/freshclam-update-{failed,ok}
+
+* Tue Apr 19 2005 Charlie Brady <charlieb@e-smith.com>
+- [0.0.1-06]
+- Add missing log directories.
+
+* Tue Apr 19 2005 Charlie Brady <charlieb@e-smith.com>
+- [0.0.1-05]
+- Don't start freshclam or clamd until after bootstrap-console.
+- Move all symlink creation into createlinks script.
+
+* Mon Mar 21 2005 Charlie Brady <charlieb@e-smith.com>
+- [0.0.1-04]
+- Fix data directory in freshclam configuration.
+
+* Fri Feb 18 2005 Charlie Brady <charlieb@e-smith.com> 0.0.1-03
+- Config fixes for clamav-0.83
+
+* Wed Feb 09 2005 Charlie Brady <charlieb@e-smith.com>
+- [0.0.1-02]
+- Add Requires headers for clamd and clamav-db.
+
+* Wed Feb 09 2005 Charlie Brady <charlieb@e-smith.com>
+- [0.0.1-01]
+- Initial
+
+%prep
+%setup
+
+%build
+perl createlinks
+touch root/var/service/freshclam/down
+touch root/var/service/clamd/down
+mkdir -p root/var/log/clamd root/var/log/freshclam
+
+%pre
+
+%post
+
+%install
+rm -rf $RPM_BUILD_ROOT
+(cd root ; find . -depth -print | cpio -dump $RPM_BUILD_ROOT)
+/sbin/e-smith/genfilelist $RPM_BUILD_ROOT \
+  --file /var/service/freshclam/run 'attr(0755,root,root)' \
+  --file /var/service/freshclam/log/run 'attr(0755,root,root)' \
+  --dir /var/log/freshclam 'attr(2750,smelog,smelog)' \
+  --file /var/service/clamd/run 'attr(0755,root,root)' \
+  --file /var/service/clamd/log/run 'attr(0755,root,root)' \
+  --dir /var/log/clamd 'attr(2750,smelog,smelog)' \
+  --file /sbin/e-smith/freshclam-update-failed 'attr(0755,root,root)' \
+  --file /sbin/e-smith/freshclam-update-ok 'attr(0755,root,root)' \
+  --file /sbin/e-smith/clamscan 'attr(0755,root,root)' \
+  > %{name}-%{version}-%{release}-filelist
+
+%clean 
+rm -rf $RPM_BUILD_ROOT
+
+%files -f %{name}-%{version}-%{release}-filelist
+%defattr(-,root,root)
---
+++ root/usr/lib/perl5/site_perl/esmith/FormMagick/Panel/clamav.pm
@@ -0,0 +1,192 @@
+#!/usr/bin/perl -w 
+
+#----------------------------------------------------------------------
+# copyright (C) 2004 Shad L. Lords <slords@mail.com>
+# Copyright (C) 2005 Gordon Rowell <gordonr@gormand.com.au>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 		
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 		
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+#----------------------------------------------------------------------
+
+package esmith::FormMagick::Panel::clamav;
+
+use strict;
+use esmith::ConfigDB;
+use esmith::FormMagick;
+use CGI::FormMagick::TagMaker;
+use esmith::util;
+use esmith::cgi;
+use File::Basename;
+use File::stat;
+use Exporter;
+use Carp;
+
+our @ISA = qw(esmith::FormMagick Exporter);
+
+our @EXPORT = qw(
+        get_prop get_value 
+        change_settings domain_name_or_ip portnumber
+        );
+
+our $VERSION = sprintf '%d.%03d', q$Revision: 1.3 $ =~ /: (\d+).(\d+)/;
+our $db = esmith::ConfigDB->open 
+|| warn "Couldn't open configuration database (permissions problems?)";
+
+=pod 
+
+=head1 NAME
+
+esmith::FormMagick::Panels::clamav - useful panel functions
+
+=head1 SYNOPSIS
+
+use esmith::FormMagick::Panels::clamav;
+
+my $panel = esmith::FormMagick::Panel::clamav->new();
+$panel->display();
+
+=head1 DESCRIPTION
+
+=cut
+
+=head2 new();
+
+Exactly as for esmith::FormMagick
+
+=cut
+
+sub new {
+    shift;
+    my $self = esmith::FormMagick->new();
+    $self->{calling_package} = (caller)[0];
+    bless $self;
+    return $self;
+}
+
+=head2 get_prop ITEM PROP
+
+A simple accessor for esmith::ConfigDB::Record::prop
+
+=cut
+
+sub get_prop {
+    my ($fm, $item, $prop, $default) = @_;
+    warn "You must specify a record key"    unless $item;
+    warn "You must specify a property name" unless $prop;
+    my $record = $db->get($item) or warn "Couldn't get record for $item";
+    my $value = $record ? $record->prop($prop) : undef;
+    return defined $value ? $value : $default;
+}
+
+=head2 get_value ITEM
+
+A simple accessor for esmith::ConfigDB::Record::value
+
+=cut
+
+sub get_value {
+    my ($fm,$item,$default) = @_;
+    my $record = $db->get($item) or warn "Couldn't get record for $item";
+    my $value = $record ? $record->value() : undef;
+    return defined $value ? $value : $default;
+}
+
+sub change_settings {
+    my ($fm) = @_;
+    my $q = $fm->{'cgi'};
+
+    my $status = $q->param('status');
+
+    my $FilesystemScan = ( $q->param('FilesystemScan') || 'disabled' );
+
+    my $DatabaseMirror = ( $q->param('DatabaseMirror') || 'db.us.clamav.net' );
+    my $UpdateOfficeHrs = ( $q->param('UpdateOfficeHrs') || 'disabled' );
+    my $UpdateNonOfficeHrs = ( $q->param('UpdateNonOfficeHrs') || 'disabled' );
+    my $UpdateWeekend = ( $q->param('UpdateWeekend') || 'disabled' );
+
+    my $HTTPProxyServer = ( $q->param('HTTPProxyServer') || '' );
+    my $HTTPProxyPort = ( $q->param('HTTPProxyPort') || '' );
+    my $HTTPProxyUsername = ( $q->param('HTTPProxyUsername') || '' );
+    my $HTTPProxyPassword = ( $q->param('HTTPProxyPassword') || '' );
+
+    my $clamav = $db->get('clamav') || $db->new_record('clamav', {type=>'service'});
+    $status ||= $clamav->prop('status');
+
+    $clamav->merge_props(
+	    status => $status,
+	    FilesystemScan => $FilesystemScan,
+	    DatabaseMirror => $DatabaseMirror,
+	    UpdateOfficeHrs => $UpdateOfficeHrs,
+	    UpdateNonOfficeHrs => $UpdateNonOfficeHrs,
+	    UpdateWeekend => $UpdateWeekend,
+	    HTTPProxyServer => $HTTPProxyServer,
+	    HTTPProxyPort => $HTTPProxyPort,
+	    HTTPProxyUsername => $HTTPProxyUsername,
+	    HTTPProxyPassword => $HTTPProxyPassword,
+	);
+
+    return $fm->error('ERROR_UPDATING')
+        unless ( system( "/sbin/e-smith/signal-event", "clamav-update" ) == 0 );
+
+    $fm->success('SUCCESS');
+}
+
+sub get_clam_versions
+{
+    my ($fm) = @_;
+
+    my $version = `/usr/bin/clamscan -V`;
+    chomp $version;
+    $version =~ s/^ClamAV //;
+ 
+    return $version;
+}
+
+sub domain_name_or_ip {
+    my ($fm, $data) = @_;
+
+    return 'OK' unless $data;
+    my $response = $fm->domain_name($data);
+    $response = $fm->ip_number($data) unless ($response eq 'OK');
+    $response = 'INVALID_PROXY_SERVER' unless ($response eq 'OK');
+    return $response;
+}
+
+sub portnumber
+{
+    my ($fm, $data) = @_;
+
+    return 'OK' unless $data;
+    my $response = $fm->number($data);
+    $response = 'INVALID_PORT_NUMBER' unless ($response eq 'OK');
+    return $response;
+}
+
+sub show_proxy_settings
+{
+    my ($fm, $data) = @_;
+
+    return (($db->get_prop("clamav", "ShowProxySettings") || "no") eq "yes");
+}
+
+sub show_update_settings
+{
+    my ($fm, $data) = @_;
+
+    return (($db->get_prop("clamav", "ShowUpdateSettings") || "no") eq "yes");
+}
+
+1;
+
+
